29900d5ca4992590c89cf6090639b7d25a770f2d
s-toshev <simeon.toshev@yahoo.bg>
2024-01-26 18:34:18 +0200

    new

--- BackEndBasics/UnitTestingJSLab/UnitTestingLab1/test/sumNumbersTest.mjs
+++ BackEndBasics/UnitTestingJSLab/UnitTestingLab1/test/sumNumbersTest.mjs
@@ -2,16 +2,32 @@ import { expect } from 'chai';
 import { sum } from '../sumNumbers.js'; 
 
 describe('Sum numbers', () => {
-    it('sums single number', () => {
-        expect(sum([1])).to.equal(1);
+    it('should return 0 if an empty array is given', () => {
+        //arrange
+            const inputArray = [];
+        //act
+            const result = sum(inputArray);
+        //assert
+        expect(result).to.equals(0);
     });
 
     // Test overloading
-    it('sums multiple numbers', () => {
-        expect(sum([1,1])).to.equal(2);
+    it('should return the sinle element as a sum, if single element array is given', () => {
+        //arrange
+            const inputArray = [33];
+        //act
+            const result = sum(inputArray);
+        //assert
+            expect(result).to.equals(33);
     });
 
-    it('sums different numbers', () => {
-        expect(sum([2,3,4])).to.equal(9);
+    it('should return the total sum o farr if multi value array is given', () => {
+             //arrange
+             const inputArray = [3,3,4];
+             //act
+                 const result = sum(inputArray);
+             //assert
+                 expect(result).to.equals(10);
+
     });
 });
